4e30995eb406a2e0cfaa0e19594b2f01e7bd588a
kem3a <kem_3a@hotmail.com>
2022-07-02 07:28:42 +0300

    Revert "minor fixes"
    This reverts commit 1bab6a39e8532daec403da02486fe82e4d06b7dc.

--- app.py
+++ app.py
@@ -32,9 +32,7 @@ api.add_resource(Item, "/item/<string:name>")
 api.add_resource(Store, "/store/<string:name>")    
 api.add_resource(UserRegister, "/register")
 
-db.init_app(app)
-
-
 if __name__ == "__main__":
-
+    
+    db.init_app(app)
     app.run()
